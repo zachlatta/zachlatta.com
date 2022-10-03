3fffe23b56841eaeec0de189793252ecf8698ed2
Zach Latta <zach@zachlatta.com>
2022-10-03 12:33:07 +0000

    Convert note filenames to kebab-case, support frontmatter

--- markdown-idea/filter.sh
+++ markdown-idea/filter.sh
@@ -2,12 +2,16 @@ MD_SRC=$HOME/pokedex-synced/txt/obsidian
 MD_DEST=$HOME/dev/zachlatta.com/markdown-idea/dest
 KEYWORD="#public"
 
+## set up git repo in destination if needed ##
+
 if [ ! -d "$MD_DEST/.git" ]; then
     echo "git repo not initialized in $MD_DEST, initializing!"
     pushd "$MD_DEST" > /dev/null
     git init
     popd > /dev/null
 fi
+
+## copy all files and folders that match KEYWORD into dest ##
 
 MATCHING_FILES=$(grep "$KEYWORD" "$MD_SRC" --exclude-dir='.*' -lR)
 
@@ -28,10 +32,64 @@ then
         cat "$FILE" |
             sed "s/$KEYWORD//g" \
             > "$MD_DEST/$RELATIVE_PATH"
+        
+        echo "Copied $RELATIVE_PATH into dest"
     done <<< "$MATCHING_FILES"
 else
     echo "No matching files!"
 fi
+
+## convert filenames and directories to kebab-case, add titles to frontmatter ##
+# modified from https://stackoverflow.com/a/72184470
+
+pushd "$MD_DEST" > /dev/null
+
+tmp="$(mktemp --directory)"
+
+# iterate though all files in $MD_DEST that aren't hidden, and ignore current folder (".")
+while IFS= read -r file
+do
+    title=$(basename "$file" | sed 's/\.md$//')
+    newfile=$(echo "$file" |
+        sed -e 's/\.md$//' \
+            -e 's/.*/\L&/g' \
+            -e '/[[:punct:]]*/{ s/[^[:alnum:][:space:]\/]//g}' \
+            -e 's/ \+/-/g'
+    )
+    ext=""
+
+    # if it's not a directory, then get the file extension
+    if [ ! -d "$file" ]; then
+        ext=".${file##*.}"
+    fi
+
+    newfile="$tmp/${newfile}${ext}"
+
+    # if directory, create directory. if file, copy file
+    if [ -d "$file" ]; then
+        mkdir -v "$newfile"
+    else
+        cp -v -- "$file" "$newfile"
+    fi
+
+    # if file doesn't start with ---, then add empty frontmatter!
+    if [ ! -d "$newfile" ]; then
+        if [[ ! $(sed -n '1{/^---/p};q' "$newfile") ]]; then
+            sed -i '1s/^/---\n---/' "$newfile"
+        fi
+
+        yq --front-matter="process" ".title = \"$title\"" -i "$newfile"
+    fi
+done <<< "$(find . -mindepth 1 -not -path '*/.*')"
+
+# clear old $MD_DEST, copy formatted and renamed files from tmp over, remove tmp
+rm -rf *
+cp -R $tmp/* .
+rm -rf $tmp
+
+popd > /dev/null
+
+## commit! ##
 
 pushd "$MD_DEST" > /dev/null
 git add .

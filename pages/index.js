64de5682df953e52b668a49fa7c2e0503e284061
Zach Latta <zach@zachlatta.com>
2022-10-08 13:42:06 +0000

    Add list of posts to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { getAllEntryIds } from '../lib/entries'
 
-export default function Home() {
+export default function Home({ allEntryIDs }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +16,24 @@ export default function Home() {
         </h1>
       </main>
 
+      {allEntryIDs.map((params) => (
+        <li key={params.params.entry}>
+          <Link href={`/${params.params.entry}`}>
+            <a>{params.params.entry}</a>
+          </Link>
+        </li>
+      ))}
+
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const allEntryIDs = await getAllEntryIds()
+
+  return {
+    props: {
+      allEntryIDs
+    }
+  }
 }

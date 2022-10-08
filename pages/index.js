29b98b5f58cc8c75f819c6df1714df51fa6fa504
Zach Latta <zach@zachlatta.com>
2022-10-08 18:48:48 +0000

    Add some styling to homepage

--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,35 @@ import { getAllNoteIDs } from '../lib/notes'
 
 export default function Home({ allEntryIDs }) {
   return (
-    <div>
+    <div class="container max-w-4xl mt-12 mx-auto space-y-1">
       <Head>
         <title>Zach Latta</title>
       </Head>
 
-      <main>
-        <h1>
-          Zach Latta
-        </h1>
-        <p>
-          these days i'm living in vermont. coding profoundly changed my life,
-          and i'm trying to help more teenagers have that same experience.
-        </p>
+      <h1 class="font-medium leading-tight text-2xl mt-0 mb-2">
+        Zach Latta
+      </h1>
+      <p>
+        these days i'm living in vermont. coding profoundly changed my life,
+        and i'm trying to help more teenagers have that same experience.
+      </p>
 
-        <p>
-          beyond hack club, i enjoy reading, making computer projects, and deep
-          conversations.
-        </p>
-      </main>
+      <p>
+        beyond hack club, i enjoy reading, making computer projects, and deep
+        conversations.
+      </p>
 
       <p>read posts:</p>
 
-      {allEntryIDs.map((params) => (
-        <li key={params.params.note}>
-          <Link href={`/${params.params.note}`}>
-            <a>{params.params.note}</a>
-          </Link>
-        </li>
-      ))}
+      <ul class="list-disc list-inside">
+        {allEntryIDs.map((params) => (
+          <li key={params.params.note}>
+            <Link href={`/${params.params.note}`}>
+              <a class="underline hover:decoration-dashed">{params.params.note}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
 
     </div>
   )

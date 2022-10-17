4562364cf28702b4f7756bf22e9c81d27f09cf20
Zach Latta <zach@zachlatta.com>
2022-10-17 00:04:16 +0000

    Fix crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                 <Head>
                     {/* Custom EB Garamond font */}
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
 
                     <link rel="shortcut icon" href="/favicon.ico" />
